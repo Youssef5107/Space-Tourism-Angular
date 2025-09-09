3498d513d54808cc354c0c454e5e8b9a5ba36d8f
Youssef5107 <ybelalmarzouk@gmail.com>
2025-09-09 22:18:43 +0300

    add destination component folders

---
+++ src/Destination/destination.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'Destination',
+    imports:[],
+    standalone:true,
+    templateUrl:'./destination.component.html',
+    styleUrl:'./destination.component.css'
+})
+
+export class Destination{}
