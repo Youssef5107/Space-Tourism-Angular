c3ac5de0bb59169dc741cd94899361158572c4b4
Youssef5107 <ybelalmarzouk@gmail.com>
2025-09-11 21:15:54 +0300

    chore: add europa component

---
+++ src/app/destination/europa/europa.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DestinationSubHeaderComponent } from '../destination-sub-header/destination-sub-header.component';
+
+@Component({
+  selector: 'app-europa',
+  standalone: true,
+  imports: [DestinationSubHeaderComponent],
+  templateUrl: './europa.component.html',
+  styleUrl: '../destination.component.css'
+})
+export class EuropaComponent {
+
+}
