b174c023f4c24536f02ca8714466c57762240bef
Youssef5107 <ybelalmarzouk@gmail.com>
2025-09-11 21:18:25 +0300

    chore: add destination component and destination nested components routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { DestinationComponent } from './destination/destination.component';
+import { HomeComponent } from './home/home.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { MoonComponent } from './destination/moon/moon.component';
+import { MarsComponent } from './destination/mars/mars.component';
+import { EuropaComponent } from './destination/europa/europa.component';
+import { TitanComponent } from './destination/titan/titan.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component:HomeComponent},
+    {path:'', redirectTo:'home', pathMatch:'full'}, 
+    {path:'destination', component:DestinationComponent,
+         children:[
+            {path:'moon', component:MoonComponent},
+            {path:'', redirectTo:'moon', pathMatch:'full'},
+            {path:'mars', component:MarsComponent},
+            {path:'europa', component:EuropaComponent},
+            {path:'titan', component:TitanComponent},
+            // {path:'**', redirectTo:'moon', pathMatch:'full'}
+        ]},
+    {path:'crew', component:CrewComponent},
+    {path:'technology', component:TechnologyComponent},
+    // {path:'**', redirectTo:'home', pathMatch:'full'}
+    
+];
