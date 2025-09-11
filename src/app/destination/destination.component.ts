a3bb1fbfb1208561cd2884de420fc8d07eb3a210
Youssef5107 <ybelalmarzouk@gmail.com>
2025-09-11 21:20:22 +0300

    style: add router outlet

---
+++ src/app/destination/destination.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+    selector:'app-destination',
+    imports:[RouterOutlet],
+    standalone:true,
+    templateUrl:'./destination.component.html',
+    styleUrl:'./destination.component.css'
+})
+
+export class DestinationComponent{}
